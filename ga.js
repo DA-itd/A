bd6f7d5426a31145a9e104b4a4b3640e518b0a6f
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-12-29 19:09:23 -0600

    Add files via upload

--- ga.js
+++ ga.js
@@ -1,9 +1,4 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-V2V4CT2C69"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-V2V4CT2C69');
-</script>
